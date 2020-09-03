ab5c9cfe2227915046245069fa7a60be09162933
Melanie Sumner <melaniersumner@gmail.com>
2020-09-03 13:31:47 -0500

    added extra input for keyboard navigation purposes

--- app/components/select-element.js
+++ app/components/select-element.js
@@ -4,9 +4,9 @@ import { guidFor } from '@ember/object/internals';
 export default class SelectElementComponent extends Component {
   selectId = 'select-' + guidFor(this); 
   selectOptions = [
-    { city: 'austin' },
-    { city: 'boston'},
-    { city: 'portland'}
+    { city: 'Austin' },
+    { city: 'Boston'},
+    { city: 'Portland'}
   ]; 
   selectLabelText = 'Select A City';
   selectName = 'selectCity';
